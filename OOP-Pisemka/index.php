2af3fa5708ec7e71c823e749d2263bc8dc4c80d7
Antonín Vojtěch <56150527+Tonyy1@users.noreply.github.com>
2020-12-11 02:01:47 +0100

    Update index.php

--- OOP-Pisemka/index.php
+++ OOP-Pisemka/index.php
@@ -7,7 +7,7 @@ $objekt->uloha1();
 // Ú2
 $objekt->uloha2();
 // Ú3
-$objekt->avg(6,0,0);
+echo $objekt->avg(3,5,10) . '<br>';
 // Ú4 -> part 1 a 2 
-$objekt->part1(5);
-$objekt->part2(5);
+echo $objekt->part1(6) . '<br>';
+echo $objekt->part2(5);
